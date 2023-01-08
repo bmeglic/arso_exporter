459153329ee5243559e7e2bef616fd779688e503
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2023-01-08 20:17:45 +0100

    Working release.
    Metrics readout worked only on first scrape.
    Use tokio async, reqwest async.

--- src/arso.rs
+++ src/arso.rs
@@ -1,19 +1,22 @@
-use prometheus::{self, register_int_gauge_vec, Encoder, TextEncoder};
+use prometheus::{self, register_int_gauge_vec, Encoder, TextEncoder, IntGaugeVec};
 use reqwest;
 use scraper::{
     self,
-    error::{self, SelectorErrorKind},
+    error::SelectorErrorKind,
     Html,
-    Selector};
+    Selector,
+    ElementRef,
+};
 use thiserror::Error;
+use lazy_static::lazy_static;
 
 const ARSO_URL: &str = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.html";
 
 #[derive(Error, Debug)]
 pub enum ArsoError {
-    #[error("Connection error")]
-    Connection,
-    #[error("Parsing error")]
+    #[error("Connection error: {0}")]
+    ConnectionError(String),
+    #[error("Parsing error: {0}")]
     ParseError(String),
 }
 
@@ -23,33 +26,44 @@ impl std::convert::From<SelectorErrorKind<'_>> for ArsoError {
     }
 }
 
-fn arso_get_document() -> Result<String, ArsoError> {
-    /*
-        let res = reqwest::blocking::get(ARSO_URL)?;
-
-        println!("Status: {}", res.status());
-        println!("Headers:\n{:#?}", res.headers());
-        println!("Body:\n{}", res.text()? );
-    */
-
-    let body = include_str!("../inp.html");
-    Ok(body.to_string())
+impl std::convert::From<reqwest::Error> for ArsoError {
+    fn from(err: reqwest::Error) -> Self {
+        ArsoError::ConnectionError(err.to_string())
+    }
 }
 
-pub fn arso_get_metrics() -> Result<String, ArsoError> {
-    let g_temp = register_int_gauge_vec!("arso_temperature",
-        "Temperature",
-        &["city"]).unwrap();
-    let g_rh = register_int_gauge_vec!("arso_relative_humidity",
-        "Relative humidity",
-        &["city"]).unwrap();
+#[derive(Debug)]
+struct City {
+    name: String,
+    temperature: i32,
+    relative_humidity: u32,
+}
 
-    let body = arso_get_document()?;
+async fn arso_get_document() -> Result<String, ArsoError> {
 
-    let doc = Html::parse_document(&body);
+    let body = reqwest::get(ARSO_URL)
+        .await?
+        .text()
+        .await?;
+    
+    Ok(body)
+}
+
+fn parse_datetime(input: &Html) -> Result<String, ArsoError> {
     let sel_dt = Selector::parse("th.meteoSI-header").unwrap();
-    let sel_row = Selector::parse("table.meteoSI-table > tbody > tr").unwrap();
-    let sel_city = Selector::parse("td.meteoSI-th").unwrap();
+
+    let datetime = input
+        .select(&sel_dt)
+        .next()
+        .ok_or(ArsoError::ParseError("Datetime not found".to_string()))?
+        .inner_html();
+
+    Ok(datetime)
+}
+
+fn parse_city(input: &ElementRef) -> Result<City, ArsoError> {
+
+    let sel_name = Selector::parse("td.meteoSI-th").unwrap();
     //let sel_icon = Selector::parse("td.nn_icon_wwsyn_icon").unwrap();
     let sel_temp = Selector::parse("td.t").unwrap();
     let sel_rh = Selector::parse("td.rh").unwrap();
@@ -61,38 +75,76 @@ pub fn arso_get_metrics() -> Result<String, ArsoError> {
     //let sel_g_sun_radavg = Selector::parse("td.gSunRadavg").unwrap();
     //let sel_snow = Selector::parse("td.snow").unwrap();
 
-    let node = doc.select(&sel_dt).next().unwrap();
-    dbg!(node.inner_html());
+    let name = input.select(&sel_name).next().unwrap().inner_html();
+    //let icon = node.select(&sel_icon).next().unwrap();
+    let temperature = input
+        .select(&sel_temp)
+        .next()
+        .ok_or(ArsoError::ParseError("Temperature field not found".to_string()))?
+        .inner_html()
+        .parse::<i32>()
+        .unwrap_or(-50);
+    let relative_humidity = input 
+        .select(&sel_rh)
+        .next()
+        .unwrap()
+        .inner_html()
+        .parse::<u32>()
+        .unwrap_or(0);
+    //let ddavg_icon = node.select(&sel_ddavg_icon).next().unwrap();
+    //let ffavg = node.select(&sel_ffavg).next().unwrap();
+    //let ffmax = node.select(&sel_ffmax).next().unwrap();
+    //let msl = node.select(&sel_msl).next().unwrap();
+    //let rr = node.select(&sel_rr).next().unwrap();
+    //let gsunradavg = node.select(&sel_g_sun_radavg).next().unwrap();
+    //let snow = node.select(&sel_snow).next().unwrap();
+
+//    g_temp.with_label_values(&[&city]).set(temp);
+//    g_rh.with_label_values(&[&city]).set(rh);
+    Ok(
+        City {
+            name,
+            temperature,
+            relative_humidity
+        }
+    )
+}
+
+lazy_static! {
+    pub static ref GAUGE_TEMPERATURE: IntGaugeVec = register_int_gauge_vec!(
+        "arso_temperature",
+        "Temperature",
+        &["city"]
+    ).unwrap();
+    static ref GAUGE_RH: IntGaugeVec = register_int_gauge_vec!(
+        "arso_relative_humidity",
+        "Relative humidity",
+        &["city"]
+    ).unwrap();
+}
+
+pub async fn arso_retrieve() -> Result<(), ArsoError> {
+
+    println!("Retrieving data from ARSO");
+
+    let body = arso_get_document().await?;
+    let doc = Html::parse_document(&body);
+    let sel_row = Selector::parse("table.meteoSI-table > tbody > tr").unwrap();
+
+    let dt = parse_datetime(&doc);
+    println!("Current timestamp: {}", dt?);
 
     let nodes = doc.select(&sel_row).collect::<Vec<_>>();
     for node in nodes {
-        let city = node.select(&sel_city).next().unwrap().inner_html();
-        //let icon = node.select(&sel_icon).next().unwrap();
-        let temp = node
-            .select(&sel_temp)
-            .next()
-            .unwrap()
-            .inner_html()
-            .parse::<i64>()
-            .unwrap_or(-50);
-        let rh = node
-            .select(&sel_rh)
-            .next()
-            .unwrap()
-            .inner_html()
-            .parse::<i64>()
-            .unwrap_or(-50);
-        //let ddavg_icon = node.select(&sel_ddavg_icon).next().unwrap();
-        //let ffavg = node.select(&sel_ffavg).next().unwrap();
-        //let ffmax = node.select(&sel_ffmax).next().unwrap();
-        //let msl = node.select(&sel_msl).next().unwrap();
-        //let rr = node.select(&sel_rr).next().unwrap();
-        //let gsunradavg = node.select(&sel_g_sun_radavg).next().unwrap();
-        //let snow = node.select(&sel_snow).next().unwrap();
+        let city = parse_city(&node)?;
 
-        g_temp.with_label_values(&[&city]).set(temp);
-        g_rh.with_label_values(&[&city]).set(rh);
+        GAUGE_TEMPERATURE.with_label_values(&[&city.name]).set(city.temperature as i64);
+        GAUGE_RH.with_label_values(&[&city.name]).set(city.relative_humidity as i64);
     }
+    
+    Ok(())
+}
+pub async fn arso_get_metrics() -> Result<String, ArsoError> {
 
     let encoder = TextEncoder::new();
     let mut buffer = vec![];
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@ use axum::{
     Router,
 };
 use std::net::SocketAddr;
+use tokio::time::{self, Duration};
+use tokio::task;
+
 
 mod arso;
-
-#[derive(Debug)]
-enum Error {
-    Timeout,
-}
 
 fn root() -> Router {
     async fn handler() -> &'static str {
@@ -21,7 +19,10 @@ fn root() -> Router {
 
 fn get_metrics() -> Router {
     async fn handler() -> String {
-        arso::arso_get_metrics().unwrap_or("Error\r\n".to_string())
+        match arso::arso_get_metrics().await {
+            Ok(m) => m,
+            Err(e) => e.to_string(),
+        }
     }
 
     route("/metrics", get(handler))
@@ -31,11 +32,24 @@ fn route(path: &str, method_router: MethodRouter<()>) -> Router {
     Router::new().route(path, method_router)
 }
 
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().merge(root()).merge(get_metrics());
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 9336));
+    let _forever = task::spawn(async {
+        let mut interval = time::interval(Duration::from_secs(20 * 60));
+
+        loop {
+            interval.tick().await;
+            let _retrieve = arso::arso_retrieve().await;
+        }
+    });
+
+    let app = Router::new()
+        .merge(root())
+        .merge(get_metrics());
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 9336));
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
