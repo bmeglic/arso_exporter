a3c3d8d878aea9a916b033e3dfabf9edc925cb99
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2023-01-17 20:37:43 +0100

    Add configuration file, additional metrics, remove metric if not able to parse

--- src/arso.rs
+++ src/arso.rs
@@ -1,14 +1,12 @@
-use prometheus::{self, register_int_gauge_vec, Encoder, TextEncoder, IntGaugeVec};
-use reqwest;
-use scraper::{
-    self,
-    error::SelectorErrorKind,
-    Html,
-    Selector,
-    ElementRef,
-};
-use thiserror::Error;
+use std::num::{ParseFloatError, ParseIntError};
+
 use lazy_static::lazy_static;
+use prometheus::{
+    self, register_gauge_vec, register_int_gauge_vec, Encoder, GaugeVec, IntGaugeVec, TextEncoder,
+};
+use reqwest;
+use scraper::{self, error::SelectorErrorKind, ElementRef, Html, Selector};
+use thiserror::Error;
 
 const ARSO_URL: &str = "https://meteo.arso.gov.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.html";
 
@@ -35,17 +33,18 @@ impl std::convert::From<reqwest::Error> for ArsoError {
 #[derive(Debug)]
 struct City {
     name: String,
-    temperature: i32,
-    relative_humidity: u32,
+    temperature: Result<i32, ParseIntError>,
+    relative_humidity: Result<u32, ParseIntError>,
+    wind_avg: Result<u32, ParseIntError>,
+    rainfall: Result<f64, ParseFloatError>,
+    solar: Result<u32, ParseIntError>,
+    snowfall: Result<u32, ParseIntError>,
 }
 
 async fn arso_get_document() -> Result<String, ArsoError> {
+    let body = reqwest::get(ARSO_URL).await?.text().await?;
+    //let body = include_str!("../inp.html").to_string();
 
-    let body = reqwest::get(ARSO_URL)
-        .await?
-        .text()
-        .await?;
-    
     Ok(body)
 }
 
@@ -62,69 +61,98 @@ fn parse_datetime(input: &Html) -> Result<String, ArsoError> {
 }
 
 fn parse_city(input: &ElementRef) -> Result<City, ArsoError> {
-
     let sel_name = Selector::parse("td.meteoSI-th").unwrap();
     //let sel_icon = Selector::parse("td.nn_icon_wwsyn_icon").unwrap();
     let sel_temp = Selector::parse("td.t").unwrap();
     let sel_rh = Selector::parse("td.rh").unwrap();
     //let sel_ddavg_icon = Selector::parse("td.ddavg_icon").unwrap();
-    //let sel_ffavg = Selector::parse("td.ffavg_val").unwrap();
+    let sel_ffavg = Selector::parse("td.ffavg_val").unwrap();
     //let sel_ffmax = Selector::parse("td.ffmax_val").unwrap();
     //let sel_msl = Selector::parse("td.msl").unwrap();
-    //let sel_rr = Selector::parse("td.rr_val").unwrap();
-    //let sel_g_sun_radavg = Selector::parse("td.gSunRadavg").unwrap();
-    //let sel_snow = Selector::parse("td.snow").unwrap();
+    let sel_rr = Selector::parse("td.rr_val").unwrap();
+    let sel_g_sun_radavg = Selector::parse("td.gSunRadavg").unwrap();
+    let sel_snow = Selector::parse("td.snow").unwrap();
 
     let name = input.select(&sel_name).next().unwrap().inner_html();
     //let icon = node.select(&sel_icon).next().unwrap();
     let temperature = input
         .select(&sel_temp)
         .next()
-        .ok_or(ArsoError::ParseError("Temperature field not found".to_string()))?
+        .ok_or(ArsoError::ParseError(
+            "Temperature field not found".to_string(),
+        ))?
         .inner_html()
-        .parse::<i32>()
-        .unwrap_or(-50);
-    let relative_humidity = input 
+        .parse::<i32>();
+    let relative_humidity = input
         .select(&sel_rh)
         .next()
-        .unwrap()
+        .ok_or(ArsoError::ParseError(
+            "Relative humidity field not found".to_string(),
+        ))?
         .inner_html()
-        .parse::<u32>()
-        .unwrap_or(0);
+        .parse::<u32>();
     //let ddavg_icon = node.select(&sel_ddavg_icon).next().unwrap();
-    //let ffavg = node.select(&sel_ffavg).next().unwrap();
+    let wind_avg = input
+        .select(&sel_ffavg)
+        .next()
+        .ok_or(ArsoError::ParseError(
+            "Average wind field not found".to_string(),
+        ))?
+        .inner_html()
+        .parse::<u32>();
     //let ffmax = node.select(&sel_ffmax).next().unwrap();
     //let msl = node.select(&sel_msl).next().unwrap();
-    //let rr = node.select(&sel_rr).next().unwrap();
-    //let gsunradavg = node.select(&sel_g_sun_radavg).next().unwrap();
-    //let snow = node.select(&sel_snow).next().unwrap();
+    let rainfall = input
+        .select(&sel_rr)
+        .next()
+        .ok_or(ArsoError::ParseError(
+            "Rainfall field not found".to_string(),
+        ))?
+        .inner_html()
+        .parse::<f64>();
 
-//    g_temp.with_label_values(&[&city]).set(temp);
-//    g_rh.with_label_values(&[&city]).set(rh);
-    Ok(
-        City {
-            name,
-            temperature,
-            relative_humidity
-        }
-    )
+    let solar = input
+        .select(&sel_g_sun_radavg)
+        .next()
+        .ok_or(ArsoError::ParseError(
+            "Solar radiation field not found".to_string(),
+        ))?
+        .inner_html()
+        .parse::<u32>();
+    let snowfall = input
+        .select(&sel_snow)
+        .next()
+        .ok_or(ArsoError::ParseError("Snow field not found".to_string()))?
+        .inner_html()
+        .parse::<u32>();
+
+    Ok(City {
+        name,
+        temperature,
+        relative_humidity,
+        rainfall,
+        wind_avg,
+        solar,
+        snowfall,
+    })
 }
 
 lazy_static! {
-    pub static ref GAUGE_TEMPERATURE: IntGaugeVec = register_int_gauge_vec!(
-        "arso_temperature",
-        "Temperature",
-        &["city"]
-    ).unwrap();
-    static ref GAUGE_RH: IntGaugeVec = register_int_gauge_vec!(
-        "arso_relative_humidity",
-        "Relative humidity",
-        &["city"]
-    ).unwrap();
+    static ref GAUGE_TEMPERATURE: IntGaugeVec =
+        register_int_gauge_vec!("arso_temperature", "Temperature", &["city"]).unwrap();
+    static ref GAUGE_RH: IntGaugeVec =
+        register_int_gauge_vec!("arso_relative_humidity", "Relative humidity", &["city"]).unwrap();
+    static ref GAUGE_RAINFALL: GaugeVec =
+        register_gauge_vec!("arso_rainfall", "Rainfall", &["city"]).unwrap();
+    static ref GAUGE_WIND_AVG: IntGaugeVec =
+        register_int_gauge_vec!("arso_wind_average", "Average wind speed", &["city"]).unwrap();
+    static ref GAUGE_SOLAR: IntGaugeVec =
+        register_int_gauge_vec!("arso_solar_radiation", "Solar radiation", &["city"]).unwrap();
+    static ref GAUGE_SNOWFALL: IntGaugeVec =
+        register_int_gauge_vec!("arso_snowfall", "Snow fall", &["city"]).unwrap();
 }
 
-pub async fn arso_retrieve() -> Result<(), ArsoError> {
-
+pub async fn arso_retrieve(cities: &Vec<String>) -> Result<(), ArsoError> {
     println!("Retrieving data from ARSO");
 
     let body = arso_get_document().await?;
@@ -138,14 +166,60 @@ pub async fn arso_retrieve() -> Result<(), ArsoError> {
     for node in nodes {
         let city = parse_city(&node)?;
 
-        GAUGE_TEMPERATURE.with_label_values(&[&city.name]).set(city.temperature as i64);
-        GAUGE_RH.with_label_values(&[&city.name]).set(city.relative_humidity as i64);
+        if cities.contains(&city.name) {
+            if let Ok(temperature) = city.temperature {
+                GAUGE_TEMPERATURE
+                    .with_label_values(&[&city.name])
+                    .set(temperature as i64);
+            } else {
+                let _ = GAUGE_TEMPERATURE.remove_label_values(&[&city.name]);
+            }
+
+            if let Ok(relative_humidity) = city.relative_humidity {
+                GAUGE_RH
+                    .with_label_values(&[&city.name])
+                    .set(relative_humidity as i64);
+            } else {
+                let _ = GAUGE_RH.remove_label_values(&[&city.name]);
+            }
+
+            if let Ok(rainfall) = city.rainfall {
+                GAUGE_RAINFALL
+                    .with_label_values(&[&city.name])
+                    .set(rainfall);
+            } else {
+                let _ = GAUGE_RAINFALL.remove_label_values(&[&city.name]);
+            }
+
+            if let Ok(wind_avg) = city.wind_avg {
+                GAUGE_WIND_AVG
+                    .with_label_values(&[&city.name])
+                    .set(wind_avg as i64);
+            } else {
+                let _ = GAUGE_WIND_AVG.remove_label_values(&[&city.name]);
+            }
+
+            if let Ok(solar) = city.solar {
+                GAUGE_SOLAR
+                    .with_label_values(&[&city.name])
+                    .set(solar as i64);
+            } else {
+                let _ = GAUGE_SOLAR.remove_label_values(&[&city.name]);
+            }
+
+            if let Ok(snowfall) = city.snowfall {
+                GAUGE_SNOWFALL
+                    .with_label_values(&[&city.name])
+                    .set(snowfall as i64);
+            } else {
+                let _ = GAUGE_SNOWFALL.remove_label_values(&[&city.name]);
+            }
+        }
     }
-    
+
     Ok(())
 }
 pub async fn arso_get_metrics() -> Result<String, ArsoError> {
-
     let encoder = TextEncoder::new();
     let mut buffer = vec![];
     encoder
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,27 @@ use axum::{
     routing::{get, MethodRouter},
     Router,
 };
+use config::{Config, ConfigError};
+use serde::Deserialize;
 use std::net::SocketAddr;
-use tokio::time::{self, Duration};
 use tokio::task;
-
+use tokio::time::{self, Duration};
 
 mod arso;
+
+#[derive(Debug, Deserialize)]
+struct Settings {
+    cities: Vec<String>,
+}
+
+impl Settings {
+    pub fn new() -> Result<Self, ConfigError> {
+        Config::builder()
+            .add_source(config::File::with_name("settings"))
+            .build()?
+            .try_deserialize()
+    }
+}
 
 fn root() -> Router {
     async fn handler() -> &'static str {
@@ -32,22 +47,24 @@ fn route(path: &str, method_router: MethodRouter<()>) -> Router {
     Router::new().route(path, method_router)
 }
 
-
 #[tokio::main]
 async fn main() {
+    let settings = Settings::new().unwrap();
 
-    let _forever = task::spawn(async {
-        let mut interval = time::interval(Duration::from_secs(20 * 60));
+    let _forever = task::spawn(async move {
+        let mut interval = time::interval(Duration::from_secs(10 * 60));
 
         loop {
             interval.tick().await;
-            let _retrieve = arso::arso_retrieve().await;
+            let status = arso::arso_retrieve(&settings.cities).await;
+            match status {
+                Err(err) => println!("{}", err.to_string()),
+                _ => (),
+            };
         }
     });
 
-    let app = Router::new()
-        .merge(root())
-        .merge(get_metrics());
+    let app = Router::new().merge(root()).merge(get_metrics());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 9336));
     axum::Server::bind(&addr)
