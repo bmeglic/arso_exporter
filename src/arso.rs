6e1046695555b320c619abe2e76fac690ed7a4f1
Bostjan Meglic <103102696+bmeglic@users.noreply.github.com>
2023-02-02 18:00:57 +0100

    Refactor, use a list of measurements to capture

--- src/arso.rs
+++ src/arso.rs
@@ -1,9 +1,7 @@
 use std::num::{ParseFloatError, ParseIntError};
 
 use lazy_static::lazy_static;
-use prometheus::{
-    self, register_gauge_vec, register_int_gauge_vec, Encoder, GaugeVec, IntGaugeVec, TextEncoder,
-};
+use prometheus::{self, register_gauge_vec, Encoder, GaugeVec, TextEncoder};
 use reqwest;
 use scraper::{self, error::SelectorErrorKind, ElementRef, Html, Selector};
 use thiserror::Error;
@@ -30,15 +28,80 @@ impl std::convert::From<reqwest::Error> for ArsoError {
     }
 }
 
-#[derive(Debug)]
+impl std::convert::From<ParseIntError> for ArsoError {
+    fn from(err: ParseIntError) -> Self {
+        ArsoError::ParseError(err.to_string())
+    }
+}
+
+impl std::convert::From<ParseFloatError> for ArsoError {
+    fn from(err: ParseFloatError) -> Self {
+        ArsoError::ParseError(err.to_string())
+    }
+}
+
+#[derive(Default, Debug)]
 struct City {
     name: String,
-    temperature: Result<i32, ParseIntError>,
-    relative_humidity: Result<u32, ParseIntError>,
-    wind_avg: Result<u32, ParseIntError>,
-    rainfall: Result<f64, ParseFloatError>,
-    solar: Result<u32, ParseIntError>,
-    snowfall: Result<u32, ParseIntError>,
+    temperature: Option<f64>,
+    relative_humidity: Option<f64>,
+    wind_avg: Option<f64>,
+    wind_max: Option<f64>,
+    rainfall: Option<f64>,
+    solar: Option<f64>,
+    snowfall: Option<f64>,
+}
+
+impl City {
+    fn new() -> Self {
+        City {
+            ..Default::default()
+        }
+    }
+
+    fn set_temperature(city: &mut Self, value: Option<f64>) {
+        city.temperature = value;
+    }
+    fn set_relative_humidity(city: &mut Self, value: Option<f64>) {
+        city.relative_humidity = value;
+    }
+    fn set_wind_avg(city: &mut Self, value: Option<f64>) {
+        city.wind_avg = value;
+    }
+    fn set_wind_max(city: &mut Self, value: Option<f64>) {
+        city.wind_max = value;
+    }
+    fn set_rainfall(city: &mut Self, value: Option<f64>) {
+        city.rainfall = value;
+    }
+    fn set_solar(city: &mut Self, value: Option<f64>) {
+        city.solar = value;
+    }
+    fn set_snowfall(city: &mut Self, value: Option<f64>) {
+        city.snowfall = value;
+    }
+
+    fn get_temperature(city: &Self) -> Option<f64> {
+        city.temperature
+    }
+    fn get_relative_humidity(city: &Self) -> Option<f64> {
+        city.relative_humidity
+    }
+    fn get_wind_avg(city: &Self) -> Option<f64> {
+        city.wind_avg
+    }
+    fn get_wind_max(city: &Self) -> Option<f64> {
+        city.wind_max
+    }
+    fn get_rainfall(city: &Self) -> Option<f64> {
+        city.rainfall
+    }
+    fn get_solar(city: &Self) -> Option<f64> {
+        city.solar
+    }
+    fn get_snowfall(city: &Self) -> Option<f64> {
+        city.snowfall
+    }
 }
 
 async fn arso_get_document() -> Result<String, ArsoError> {
@@ -60,96 +123,95 @@ fn parse_datetime(input: &Html) -> Result<String, ArsoError> {
     Ok(datetime)
 }
 
-fn parse_city(input: &ElementRef) -> Result<City, ArsoError> {
-    let sel_name = Selector::parse("td.meteoSI-th").unwrap();
-    //let sel_icon = Selector::parse("td.nn_icon_wwsyn_icon").unwrap();
-    let sel_temp = Selector::parse("td.t").unwrap();
-    let sel_rh = Selector::parse("td.rh").unwrap();
-    //let sel_ddavg_icon = Selector::parse("td.ddavg_icon").unwrap();
-    let sel_ffavg = Selector::parse("td.ffavg_val").unwrap();
-    //let sel_ffmax = Selector::parse("td.ffmax_val").unwrap();
-    //let sel_msl = Selector::parse("td.msl").unwrap();
-    let sel_rr = Selector::parse("td.rr_val").unwrap();
-    let sel_g_sun_radavg = Selector::parse("td.gSunRadavg").unwrap();
-    let sel_snow = Selector::parse("td.snow").unwrap();
-
-    let name = input.select(&sel_name).next().unwrap().inner_html();
-    //let icon = node.select(&sel_icon).next().unwrap();
-    let temperature = input
-        .select(&sel_temp)
-        .next()
-        .ok_or(ArsoError::ParseError(
-            "Temperature field not found".to_string(),
-        ))?
-        .inner_html()
-        .parse::<i32>();
-    let relative_humidity = input
-        .select(&sel_rh)
-        .next()
-        .ok_or(ArsoError::ParseError(
-            "Relative humidity field not found".to_string(),
-        ))?
-        .inner_html()
-        .parse::<u32>();
-    //let ddavg_icon = node.select(&sel_ddavg_icon).next().unwrap();
-    let wind_avg = input
-        .select(&sel_ffavg)
-        .next()
-        .ok_or(ArsoError::ParseError(
-            "Average wind field not found".to_string(),
-        ))?
-        .inner_html()
-        .parse::<u32>();
-    //let ffmax = node.select(&sel_ffmax).next().unwrap();
-    //let msl = node.select(&sel_msl).next().unwrap();
-    let rainfall = input
-        .select(&sel_rr)
-        .next()
-        .ok_or(ArsoError::ParseError(
-            "Rainfall field not found".to_string(),
-        ))?
-        .inner_html()
-        .parse::<f64>();
-
-    let solar = input
-        .select(&sel_g_sun_radavg)
-        .next()
-        .ok_or(ArsoError::ParseError(
-            "Solar radiation field not found".to_string(),
-        ))?
-        .inner_html()
-        .parse::<u32>();
-    let snowfall = input
-        .select(&sel_snow)
-        .next()
-        .ok_or(ArsoError::ParseError("Snow field not found".to_string()))?
-        .inner_html()
-        .parse::<u32>();
-
-    Ok(City {
-        name,
-        temperature,
-        relative_humidity,
-        rainfall,
-        wind_avg,
-        solar,
-        snowfall,
-    })
+//#[derive()]
+struct ArsoField {
+    selector_tag: String,
+    field_name: String,
+    set_fn: fn(&mut City, Option<f64>),
+    get_fn: fn(&City) -> Option<f64>,
+    metric: GaugeVec,
 }
 
 lazy_static! {
-    static ref GAUGE_TEMPERATURE: IntGaugeVec =
-        register_int_gauge_vec!("arso_temperature", "Temperature", &["city"]).unwrap();
-    static ref GAUGE_RH: IntGaugeVec =
-        register_int_gauge_vec!("arso_relative_humidity", "Relative humidity", &["city"]).unwrap();
-    static ref GAUGE_RAINFALL: GaugeVec =
-        register_gauge_vec!("arso_rainfall", "Rainfall", &["city"]).unwrap();
-    static ref GAUGE_WIND_AVG: IntGaugeVec =
-        register_int_gauge_vec!("arso_wind_average", "Average wind speed", &["city"]).unwrap();
-    static ref GAUGE_SOLAR: IntGaugeVec =
-        register_int_gauge_vec!("arso_solar_radiation", "Solar radiation", &["city"]).unwrap();
-    static ref GAUGE_SNOWFALL: IntGaugeVec =
-        register_int_gauge_vec!("arso_snowfall", "Snow fall", &["city"]).unwrap();
+    static ref ARSO_FIELDS: Vec<ArsoField> = vec!(
+        ArsoField {
+            selector_tag: "td.t".to_string(),
+            field_name: "Temperature".to_string(),
+            set_fn: City::set_temperature,
+            get_fn: City::get_temperature,
+            metric: register_gauge_vec!("arso_temperature", "Temperature", &["city"]).unwrap(),
+        },
+        ArsoField {
+            selector_tag: "td.rh".to_string(),
+            field_name: "Relative humidity".to_string(),
+            set_fn: City::set_relative_humidity,
+            get_fn: City::get_relative_humidity,
+            metric: register_gauge_vec!("arso_relative_humidity", "Relative humidity", &["city"])
+                .unwrap(),
+        },
+        ArsoField {
+            selector_tag: "td.ffavg_val".to_string(),
+            field_name: "Average wind speed".to_string(),
+            set_fn: City::set_wind_avg,
+            get_fn: City::get_wind_avg,
+            metric: register_gauge_vec!("arso_wind_avg", "Average wind speed", &["city"]).unwrap(),
+        },
+        ArsoField {
+            selector_tag: "td.ffmax_val".to_string(),
+            field_name: "Max wind speed".to_string(),
+            set_fn: City::set_wind_max,
+            get_fn: City::get_wind_max,
+            metric: register_gauge_vec!("arso_wind_max", "Maximum wind speed", &["city"]).unwrap(),
+        },
+        ArsoField {
+            selector_tag: "td.rr_val".to_string(),
+            field_name: "Rainfall".to_string(),
+            set_fn: City::set_rainfall,
+            get_fn: City::get_rainfall,
+            metric: register_gauge_vec!("arso_rainfall", "Rainfall", &["city"]).unwrap(),
+        },
+        ArsoField {
+            selector_tag: "td.gSunRadavg".to_string(),
+            field_name: "Solar radiation".to_string(),
+            set_fn: City::set_solar,
+            get_fn: City::get_solar,
+            metric: register_gauge_vec!("arso_solar_radiation", "Solar radiation", &["city"])
+                .unwrap(),
+        },
+        ArsoField {
+            selector_tag: "td.snow".to_string(),
+            field_name: "Snow blanket depth".to_string(),
+            set_fn: City::set_snowfall,
+            get_fn: City::get_snowfall,
+            metric: register_gauge_vec!("arso_snowfall", "Snow blanket depth", &["city"]).unwrap(),
+        }
+    );
+}
+
+fn parse_city(input: &ElementRef) -> Result<City, ArsoError> {
+    let mut city = City::new();
+
+    let sel_name = Selector::parse("td.meteoSI-th").unwrap();
+    let name = input.select(&sel_name).next().unwrap().inner_html();
+    city.name = name;
+
+    for field in ARSO_FIELDS.iter() {
+        let selector = Selector::parse(&field.selector_tag).unwrap();
+        let val = input
+            .select(&selector)
+            .next()
+            .ok_or(ArsoError::ParseError(format!(
+                "Field '{}' not found",
+                field.field_name
+            )))?
+            .inner_html()
+            .parse::<f64>()
+            .ok();
+
+        (field.set_fn)(&mut city, val);
+    }
+
+    Ok(city)
 }
 
 pub async fn arso_retrieve(cities: &Vec<String>) -> Result<(), ArsoError> {
@@ -167,52 +229,14 @@ pub async fn arso_retrieve(cities: &Vec<String>) -> Result<(), ArsoError> {
         let city = parse_city(&node)?;
 
         if cities.contains(&city.name) {
-            if let Ok(temperature) = city.temperature {
-                GAUGE_TEMPERATURE
-                    .with_label_values(&[&city.name])
-                    .set(temperature as i64);
-            } else {
-                let _ = GAUGE_TEMPERATURE.remove_label_values(&[&city.name]);
-            }
+            for field in ARSO_FIELDS.iter() {
+                let val = (field.get_fn)(&city);
 
-            if let Ok(relative_humidity) = city.relative_humidity {
-                GAUGE_RH
-                    .with_label_values(&[&city.name])
-                    .set(relative_humidity as i64);
-            } else {
-                let _ = GAUGE_RH.remove_label_values(&[&city.name]);
-            }
-
-            if let Ok(rainfall) = city.rainfall {
-                GAUGE_RAINFALL
-                    .with_label_values(&[&city.name])
-                    .set(rainfall);
-            } else {
-                let _ = GAUGE_RAINFALL.remove_label_values(&[&city.name]);
-            }
-
-            if let Ok(wind_avg) = city.wind_avg {
-                GAUGE_WIND_AVG
-                    .with_label_values(&[&city.name])
-                    .set(wind_avg as i64);
-            } else {
-                let _ = GAUGE_WIND_AVG.remove_label_values(&[&city.name]);
-            }
-
-            if let Ok(solar) = city.solar {
-                GAUGE_SOLAR
-                    .with_label_values(&[&city.name])
-                    .set(solar as i64);
-            } else {
-                let _ = GAUGE_SOLAR.remove_label_values(&[&city.name]);
-            }
-
-            if let Ok(snowfall) = city.snowfall {
-                GAUGE_SNOWFALL
-                    .with_label_values(&[&city.name])
-                    .set(snowfall as i64);
-            } else {
-                let _ = GAUGE_SNOWFALL.remove_label_values(&[&city.name]);
+                if let Some(val) = val {
+                    field.metric.with_label_values(&[&city.name]).set(val);
+                } else {
+                    let _ = field.metric.remove_label_values(&[&city.name]);
+                }
             }
         }
     }
